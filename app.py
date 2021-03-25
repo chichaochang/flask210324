1816e4b8c553c9e0db2b22861752ef7faf742307
chichaochang <terence@mail.cjcu.edu.tw>
2021-03-25 15:52:25 +0800

    add variable.html

--- app.py
+++ app.py
@@ -35,5 +35,12 @@ def hello03(name):
     return render_template('hello3.html', **locals())
 
 
+@app.route('/variable')
+def variable():
+    student = {'學號':'874523', '姓名':'張三', '性別':'男'}
+    fruit = ['蘋果', '香蕉', '芭樂', '百香果']
+    return render_template('variable.html', **locals())
+
+
 if __name__ == '__main__':
     app.run()
